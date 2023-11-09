a9535d7d3f280e62a45edbfa8a44902404960626
Siro098 <81698835+Siro098@users.noreply.github.com>
2023-11-09 08:24:59 +0000

    Edit

---
+++ B2Bäume/Main.java
@@ -0,0 +1,22 @@
+package B2Bäume;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+Tree b1 = new Tree();
+
+b1.insert(9);
+b1.insert(10);
+b1.insert(17);
+b1.insert(5);
+b1.insert(2);
+b1.insert(1);
+b1.insert(8);
+
+
+
+System.out.println();
+
+    }
+}
---
+++ B2Bäume/Node.java
@@ -0,0 +1,15 @@
+package B2Bäume;
+
+public class Node {
+
+    public Node links;
+    public Node rechts;
+    public int Wert;
+
+    public Node(int i) {
+
+        this.Wert = i;
+
+    }
+
+}
